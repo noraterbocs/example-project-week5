1db903241d6f7a6fd833fcdbe83259c994a7cb96
dmnich <100215029+dmnich@users.noreply.github.com>
2022-09-19 17:32:20 +0200

    Init

---
+++ code/src/index.js
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom';
+import './index.css';
+import { App } from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
